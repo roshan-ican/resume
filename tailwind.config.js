aeedac60334a3c001a40b98ca55d4d3947f0fb47
roshan-ican <roshansahani535@gmail.com>
2022-11-30 00:18:07 +0530

    final try

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  darkMode: 'class',
+  content: ["./pages/*.{html,js,jsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+
+  theme: {
+    extend: {
+      fontFamily: {
+        burtons: "burtons",
+      },
+    },
+  },
+  plugins: [],
+}
