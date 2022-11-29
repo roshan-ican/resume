2568fa470e3c119eafcaf37ae57e3304f448ac0f
roshan-ican <roshansahani535@gmail.com>
2022-11-30 00:26:54 +0530

    solved the import

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import {
   AiFillInstagram,
   AiFillTwitterCircle,
 } from "react-icons/ai"
-import Image from "next/Image"
+import Image from "next/image"
 import deved from "../public/dev-ed-wave.png"
 import design from "../public/design.png"
 import code from "../public/code.png"
@@ -29,10 +29,10 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="bg-white px-10 md:px-20 lg:px-40 dark:bg-gray-900">
+      <main className="px-10 bg-white md:px-20 lg:px-40 dark:bg-gray-900">
         {/* section with navbar */}
         <section className="min-h-screen">
-          <nav className="py-10 mb-12 flex justify-between">
+          <nav className="flex justify-between py-10 mb-12">
             <h1 className="text-xl font-burtons dark:text-white">
               DevelopbyRoshan
             </h1>
@@ -40,13 +40,13 @@ export default function Home() {
               <li className="list-none">
                 <BsFillMoonStarsFill
                   onClick={() => setDarkMode(!darkMode)}
-                  className="cursor-pointer text-2xl dark:text-white"
+                  className="text-2xl cursor-pointer dark:text-white"
                 />
               </li>
               <li>
                 <a
                   href="https://docs.google.com/document/d/1O-Qtqdoo78x73hYMrL8_7dNIXXzAXaku/edit?usp=sharing&ouid=113995107266914876097&rtpof=true&sd=true"
-                  className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8"
+                  className="px-4 py-2 ml-8 text-white rounded-md bg-gradient-to-r from-cyan-500 to-teal-500"
                 >
                   Resume
                 </a>
@@ -54,19 +54,19 @@ export default function Home() {
             </ul>
           </nav>
           {/* Your Intro */}
-          <div className="text-center p-10">
-            <h2 className="text-5xl text-teal-600 font-medium md:text-6xl">
+          <div className="p-10 text-center">
+            <h2 className="text-5xl font-medium text-teal-600 md:text-6xl">
               Roshan
             </h2>
-            <h3 className="text-2xl py-2 md:text-3xl dark:text-white">
+            <h3 className="py-2 text-2xl md:text-3xl dark:text-white">
               Frontend Developer
             </h3>
-            <p className="text-md py-5 leading-8 text-gray-800 font-semibold md:text-xl max-w-xl mx-auto dark:text-white">
+            <p className="max-w-xl py-5 mx-auto font-semibold leading-8 text-gray-800 text-md md:text-xl dark:text-white">
               Freelancing providing services for programming needs. Join me
               let's turn your ideas into reality.
             </p>
           </div>
-          <div className="text-5xl flex justify-center gap-16 py-3">
+          <div className="flex justify-center gap-16 py-3 text-5xl">
             {/* <AiFillTwiiterCircle /> */}
             <Link
               legacyBehavior
@@ -92,7 +92,7 @@ export default function Home() {
               </a>
             </Link>
           </div>
-          <div className="relative bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 mx-auto overflow-hidden md:h-96 md:w-96">
+          <div className="relative mx-auto mt-20 overflow-hidden rounded-full bg-gradient-to-b from-teal-500 w-80 h-80 md:h-96 md:w-96">
             <Image src={deved} layout="fill" objectFit="cover" alt="profile" />
           </div>
         </section>
@@ -100,8 +100,8 @@ export default function Home() {
             
         <section>
           <div>
-            <h3 className="text-3xl py-1 dark:text-white">Services I offer</h3>
-            <p className="text-md py-2 leading-8 text-gray-800 dark:text-white">
+            <h3 className="py-1 text-3xl dark:text-white">Services I offer</h3>
+            <p className="py-2 leading-8 text-gray-800 text-md dark:text-white">
               Since the beginning of my journey as a freelancer developer, I've
               done remote work for various startups, mostly with MERN Stack.
               <br />
@@ -111,14 +111,14 @@ export default function Home() {
               E-commerce, Blockchain and Admin applications for both business
               and consumer use.
             </p>
-            <p className="text-md py-2 leading-8 text-gray-800 dark:text-white">
+            <p className="py-2 leading-8 text-gray-800 text-md dark:text-white">
               I offer a wide range of services from web development to digital
               marketing, consultation
             </p>
           </div>
-          <div className="lg:flex gap-10">
+          <div className="gap-10 lg:flex">
             {/* card  1*/}
-            <div className="flex flex-col text-center shadow-lg p-10 rounded-xl my-10 justify-center items-center mx-auto">
+            <div className="flex flex-col items-center justify-center p-10 mx-auto my-10 text-center shadow-lg rounded-xl">
               <Image
                 src={design}
                 width={100}
@@ -126,7 +126,7 @@ export default function Home() {
                 className="justify-center"
                 alt="designIcon"
               />
-              <h3 className="text-lg font-medium pt-8 pb-2 dark:text-white">
+              <h3 className="pt-8 pb-2 text-lg font-medium dark:text-white">
                 Beautiful Designs
               </h3>
               <p className="font-medium dark:text-white">
@@ -135,18 +135,18 @@ export default function Home() {
               <h4 className="py-4 font-bold text-teal-600">
                 Technologies that I use
               </h4>
-              <p className="text-gray-800 py-1 uppercase dark:text-white">
+              <p className="py-1 text-gray-800 uppercase dark:text-white">
                 Figma
               </p>
-              <p className="text-gray-800 py-1 uppercase dark:text-white">
+              <p className="py-1 text-gray-800 uppercase dark:text-white">
                 Photoshop CC and Coral Draw
               </p>
-              <p className="text-gray-800 py-1 uppercase dark:text-white">
+              <p className="py-1 text-gray-800 uppercase dark:text-white">
                 Illustrator
               </p>
             </div>
             {/* card 2 */}
-            <div className=" flex flex-col text-center shadow-lg p-10 rounded-xl my-10 justify-center items-center mx-auto">
+            <div className="flex flex-col items-center justify-center p-10 mx-auto my-10 text-center shadow-lg  rounded-xl">
               <Image
                 src={code}
                 width={100}
@@ -154,27 +154,27 @@ export default function Home() {
                 className="justify-center"
                 alt="codeIcon"
               />
-              <h3 className="text-lg font-medium pt-8 pb-2 dark:text-white">
+              <h3 className="pt-8 pb-2 text-lg font-medium dark:text-white">
                 Fast and Responsive Website
               </h3>
               <p className="font-medium dark:text-white">
                 Creating elegant websites suited for your needs
               </p>
-              <h4 className="py-4 text-teal-600 font-bold">
+              <h4 className="py-4 font-bold text-teal-600">
                 Technologies that I use
               </h4>
-              <p className="text-gray-800 py-1 uppercase dark:text-white">
+              <p className="py-1 text-gray-800 uppercase dark:text-white">
                 Javascript, React JS, Next Js
               </p>
-              <p className="text-gray-800 py-1 uppercase dark:text-white">
+              <p className="py-1 text-gray-800 uppercase dark:text-white">
                 HTML, CSS, BOOTSTRAP, TAILWINDCSS
               </p>
-              <p className="text-gray-800 py-1 uppercase dark:text-white">
+              <p className="py-1 text-gray-800 uppercase dark:text-white">
                 Node Js, Django, Express Js, Firebase
               </p>
             </div>
             {/*card 3 */}
-            <div className=" flex flex-col text-center shadow-lg p-10 rounded-xl my-10 justify-center items-center mx-auto">
+            <div className="flex flex-col items-center justify-center p-10 mx-auto my-10 text-center shadow-lg  rounded-xl">
               <Image
                 src={consulting}
                 width={100}
@@ -182,20 +182,20 @@ export default function Home() {
                 className="justify-center"
                 alt="designIcon"
               />
-              <h3 className="text-lg font-medium pt-8 pb-2 dark:text-white">
+              <h3 className="pt-8 pb-2 text-lg font-medium dark:text-white">
                 Consulting that helps you grow
               </h3>
               <p className="font-medium dark:text-white">
                 Experience based advices
               </p>
-              {/* <h4 className="py-4 text-teal-600 font-bold">Technologies that I use</h4>
-              <p className="text-gray-800 py-1 uppercase dark:text-white">
+              {/* <h4 className="py-4 font-bold text-teal-600">Technologies that I use</h4>
+              <p className="py-1 text-gray-800 uppercase dark:text-white">
                 Javascript, React JS, Next Js
               </p>
-              <p className="text-gray-800 py-1 uppercase dark:text-white">
+              <p className="py-1 text-gray-800 uppercase dark:text-white">
                 HTML, CSS, BOOTSTRAP, TAILWINDCSS
               </p>
-              <p className="text-gray-800 py-1 uppercase dark:text-white">
+              <p className="py-1 text-gray-800 uppercase dark:text-white">
                 Node Js, Django, Express Js, Firebase
               </p> */}
             </div>
@@ -204,8 +204,8 @@ export default function Home() {
         {/* dark mode */}
         <section>
           <div>
-            <h3 className="text-3xl py-1 dark:text-white">Portfolio</h3>
-            <p className="text-md py-2 leading-8 text-gray-800 dark:text-white">
+            <h3 className="py-1 text-3xl dark:text-white">Portfolio</h3>
+            <p className="py-2 leading-8 text-gray-800 text-md dark:text-white">
               Since the beginning of my journey as a freelancer developer, I've
               done remote work for various startups, mostly with MERN Stack.
               <span className="text-teal-500">
@@ -217,26 +217,26 @@ export default function Home() {
               Mostly E-commerce, Blockchain and Admin applications for both
               business and consumer use.
             </p>
-            <p className="text-md py-2 leading-8 text-gray-800 dark:text-white">
+            <p className="py-2 leading-8 text-gray-800 text-md dark:text-white">
               I offer a wide range of services from web development to digital
               marketing, consultation
             </p>
           </div>
           {/* projects section */}
           <div className="flex flex-col py-15 lg:flex-row lg:flex-wrap">
-            <div className="basis-1/3 flex-1 p-4">
+            <div className="flex-1 p-4 basis-1/3">
               <Link href={"https://amazona-gamma.vercel.app/"} legacyBehavior>
                 <a>
                   <Image
                     src={web1}
-                    className="rounded-lg object-cover w-full h-full"
+                    className="object-cover w-full h-full rounded-lg"
                     layout="responsive"
                     alt="Amazona"
                   />
                 </a>
               </Link>
             </div>
-            <div className="basis-1/3 flex-1 p-4">
+            <div className="flex-1 p-4 basis-1/3">
               <Link
                 href={"https://roshan-ican.github.io/SpaceX/"}
                 legacyBehavior
@@ -244,14 +244,14 @@ export default function Home() {
                 <a>
                   <Image
                     src={web2}
-                    className="rounded-lg object-cover w-full h-full"
+                    className="object-cover w-full h-full rounded-lg"
                     layout="responsive"
                     alt="Space-x"
                   />
                 </a>
               </Link>
             </div>
-            <div className="basis-1/3 flex-1 p-4">
+            <div className="flex-1 p-4 basis-1/3">
               <Link
                 href={
                   "https://roshan-ican.github.io/BootStrap-Bootcamp-website.github.io"
@@ -261,38 +261,38 @@ export default function Home() {
                 <a>
                   <Image
                     src={web3}
-                    className="rounded-lg object-cover w-full h-full"
+                    className="object-cover w-full h-full rounded-lg"
                     layout="responsive"
                     alt="BootCamp website"
                   />
                 </a>
               </Link>
             </div>
-            <div className="basis-1/3 flex-1 p-4">
+            <div className="flex-1 p-4 basis-1/3">
               <Link legacyBehavior href={"https://flickoot.netlify.app/"}>
                 <a>
                   <Image
                     src={web4}
-                    className="rounded-lg object-cover w-full h-full"
+                    className="object-cover w-full h-full rounded-lg"
                     layout="responsive"
                     alt="AOT platform"
                   />
                 </a>
               </Link>
             </div>
-            <div className="basis-1/3 flex-1 p-4">
+            <div className="flex-1 p-4 basis-1/3">
               <Link href={"#"} legacyBehavior>
                 <a>
                   <Image
                     src={web5}
-                    className="rounded-lg object-cover w-full h-full"
+                    className="object-cover w-full h-full rounded-lg"
                     layout="responsive"
                     alt="Blockchain"
                   />
                 </a>
               </Link>
             </div>
-            <div className="basis-1/3 flex-1 p-4">
+            <div className="flex-1 p-4 basis-1/3">
               <Link
                 href={"https://roshan-ican.github.io/webapp.github.io/"}
                 legacyBehavior
@@ -300,7 +300,7 @@ export default function Home() {
                 <a>
                   <Image
                     src={web6}
-                    className="rounded-lg object-cover w-full h-full"
+                    className="object-cover w-full h-full rounded-lg"
                     layout="responsive"
                     alt="Marketing-agency"
                   />
